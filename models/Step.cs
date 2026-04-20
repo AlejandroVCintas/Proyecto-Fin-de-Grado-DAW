88815016faafc730960578314fbe9e7ca1a013ec
AlejandroVCintas <alejandrovcintas@gmail.com>
2026-04-20 11:58:04 +0200

    Validaciones de datos añadidas

--- models/Step.cs
+++ models/Step.cs
@@ -1,8 +1,13 @@
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 public class Step
 {
     public int? Id { get; set; }
+
+    [Required]
     public string Date { get; set; }
+
+    [Range(1, 100000)]
     public int Steps { get; set; }
     public int? UserId { get; set; }
 
--- models/User.cs
+++ models/User.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 public class User
 {
     public int Id { get; set; }
+
+    [Required]
     public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+
+    [Required]
     public string PasswordHash { get; set; }
+
     public DateTime RegisterDate { get; set; }
     public int Weight { get; set; }
     public int Height { get; set; }
