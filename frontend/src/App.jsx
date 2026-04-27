83c5fe44092353089dca257f8be3374437a249d7
AlejandroVCintas <alejandrovcintas@gmail.com>
2026-04-27 13:09:31 +0200

    Pantalla autenticación creada y cambios en el frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Stats from "./pages/Stats";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
     <BrowserRouter>
       <nav className="navbar">
-        <Link to="/">Inicio</Link>
+        <Link to="/home">Inicio</Link>
         <Link to="/stats">Estadísticas</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Auth />} />
         <Route path="/stats" element={<Stats />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Auth.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { login, register } from "../frontservices/api";
+import { useNavigate } from "react-router-dom";
+import "./Auth.css";
+
+function Auth() {
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(true);
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (isLogin) {
+      // Login de usuario
+      await login({
+        email: form.email,
+        passwordHash: form.password,
+      });
+      alert("Login correcto");
+      // Se redirige al usuario directamente a la pestaña Inicio
+      navigate("/home");
+    } else {
+      // Registro de usuario
+      await register({
+        name: form.name,
+        email: form.email,
+        passwordHash: form.password,
+        registerDate: new Date().toISOString(),
+        weight: 0,
+        height: 0,
+      });
+      alert("Usuario creado");
+
+      setIsLogin(true);
+    }
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="auth-card">
+        <h1>{isLogin ? "Login" : "Registro"}</h1>
+
+        {/* Campo para nombre de usuario */}
+        <form onSubmit={handleSubmit}>
+          {!isLogin && (
+            <input
+              type="text"
+              placeholder="Nombre"
+              value={form.name}
+              onChange={(e) =>
+                setForm({ ...form, name: e.target.value })
+              }
+            />
+          )}
+
+        {/* Campo para el email */}
+          <input
+            type="email"
+            placeholder="Email"
+            value={form.email}
+            onChange={(e) =>
+              setForm({ ...form, email: e.target.value })
+            }
+          />
+
+        {/* Campo para la contraseña */}
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={form.password}
+            onChange={(e) =>
+              setForm({ ...form, password: e.target.value })
+            }
+          />
+
+        {/* Botón de Login/Registro */}
+          <button className="btn-primary">
+            {isLogin ? "Entrar" : "Registrarse"}
+          </button>
+        </form>
+
+        <p onClick={() => setIsLogin(!isLogin)} className="toggle">
+          {isLogin
+            ? "¿No tienes cuenta? Regístrate"
+            : "¿Ya tienes cuenta? Login"}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Auth;
