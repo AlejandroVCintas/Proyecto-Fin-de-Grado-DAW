00118f6a8cf2ed852ad81f1ff4c7f8b45b003eff
AlejandroVCintas <alejandrovcintas@gmail.com>
2026-04-27 12:34:38 +0200

    Gráfica de pasos añadida

--- frontend/src/pages/Stats.jsx
+++ frontend/src/pages/Stats.jsx
@@ -2,6 +2,17 @@ import { useEffect, useState } from "react";
 import { getToday, getWeek } from "../frontservices/api";
 import "./Stats.css";
 
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
+
 function Stats() {
   const [todaySteps, setTodaySteps] = useState(0);
   const [weekSteps, setWeekSteps] = useState([]);
@@ -24,6 +35,21 @@ function Stats() {
   // Total de pasos semanal
   const totalWeek = weekSteps.reduce((acc, s) => acc + s.steps, 0);
 
+  const chartData = {
+    labels: weekSteps.map((s) =>
+      new Date(s.date).toLocaleDateString()
+    ),
+    datasets: [
+      {
+        label: "Pasos realizados esta semana",
+        data: weekSteps.map((s) => s.steps),
+        borderColor: "#4CAF50",
+        backgroundColor: "rgba(76, 175, 80, 0.2)",
+        tension: 0.4,
+      },
+    ],
+  };
+
   return (
     <div className="stats-page">
       <h1>Estadísticas</h1>
@@ -38,6 +64,17 @@ function Stats() {
       <div className="card">
         <h2>Total semanal</h2>
         <p className="big-number">{totalWeek}</p>
+      </div>
+
+      {/* GRÁFICA PASOS SEMANAL */}
+      <div className="card">
+        <h2>Gráfica semanal</h2>
+
+        {weekSteps.length === 0 ? (
+          <p>No hay datos para mostrar</p>
+        ) : (
+          <Line data={chartData} />
+        )}
       </div>
 
       {/* LISTA PASOS SEMANAL */}
